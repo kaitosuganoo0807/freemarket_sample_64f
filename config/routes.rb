0c9c227c745f1d27a860af6cd44a4c4cdc8ae082
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-10 14:25:19 +0900

    Exhibit-view2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :mypages, only: :index
-  
+  resources :items, only: [:index, :new]
   # 仮置き
   get 'items/show', to: 'items#show'
-  get 'items/new', to: 'items#new'
 end
