2618e55b5b6da04db1586a3f6db39b2ddcadb754
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-10 13:31:16 +0900

    Add-Exhibit-view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :mypages, only: :index
-
+  
   # 仮置き
   get 'items/show', to: 'items#show'
+  get 'items/new', to: 'items#new'
 end
