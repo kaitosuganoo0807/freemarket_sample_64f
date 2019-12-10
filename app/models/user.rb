49df7c25309a88bef78dda0535bd8321ccd73b4d
osamushiota <os036@pa2.so-net.ne.jp>
2019-12-10 17:43:44 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,3 @@ class User < ApplicationRecord
   validates :nickname, :email, :password, :password_confirmation, :surname, :first_name, :surname_kana, :first_name_kana, :phone, :card_number, 
             :expiration_month, :expiration_year, :security_code, :birth_year, :birth_month, :birth_day, presence: true
 end
-
-
-
-
-
-#   validates :email, :password, :password_confirmation, presence: true
-# end
-
-
