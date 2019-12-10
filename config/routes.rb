8e690cc273ca29e784e171cfcb741edd43ed8ea2
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-10 21:40:30 +0900

    Add-Signup1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :mypages, only: :index
   resources :items, only: [:index, :new]
+  resources :signup, only: :index
   # 仮置き
   get 'items/show', to: 'items#show'
+  devise_scope :user do
+    get 'users/signup/registration', to: 'users/registrations#step1'
+  end
 end
