5e61267cbb0c5347b477525cd462e7c2c013ad6c
osamushiota <os036@pa2.so-net.ne.jp>
2019-12-11 20:00:34 +0900

    add_01

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :mypages, only: :index
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :destroy]
   resources :items, only: [:index, :new]
 
+  
+
   # 仮置き
+  get 'mypages/destroy', to: 'mypages#destroy'
   get 'items/show', to: 'items#show'
   resources :signup, only: :index do
     collection do
