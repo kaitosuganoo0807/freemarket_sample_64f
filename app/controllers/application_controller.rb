42e7309c237a156448d582ec3cc8654688299c02
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-11 16:18:13 +0900

    コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
-  before_action :authenticate_user!
-  protect_from_forgery with: :exception
+  # before_action :basic_auth, if: :production?
+  # before_action :authenticate_user!
+  # protect_from_forgery with: :exception
 
   protected
   def configure_permitted_parameters
