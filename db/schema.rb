975f8b301e9d7e81809ff1c0cf5b8dd5be7271fc
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-12 13:06:31 +0900

    Readme-change
    DB設計の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_12_030825) do
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,12 +18,12 @@ ActiveRecord::Schema.define(version: 2019_12_12_030825) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "nickname", default: ""
-    t.string "surname", default: ""
-    t.string "first_name", default: ""
-    t.string "surname_kana", default: ""
-    t.string "first_name_kana", default: ""
-    t.integer "phone"
+    t.string "nickname", null: false
+    t.string "surname", null: false
+    t.string "first_name", null: false
+    t.string "surname_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "phone", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday", null: false
