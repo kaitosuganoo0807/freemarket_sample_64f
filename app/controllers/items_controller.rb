b0c1dfe026d739dd1fd1676bf22ea146725297ea
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-12-13 17:24:09 +0900

    商品出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,33 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.build
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to controller: :items, action: :index
+    else
+      render "new"
+    end
   end
 
   def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :deteil, :category, :price, :status, :state, :city, :delivery, :delivery_time, :fee_payer, images_attributes: [:image])
+  end
+
+  def create_items_instance
+    @item = Item.new
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_030825) do
+ActiveRecord::Schema.define(version: 2019_12_13_034352) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.integer "category", null: false
+    t.integer "price", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "state", default: 0, null: false
+    t.integer "city", null: false
+    t.integer "delivery", null: false
+    t.integer "fee_payer", null: false
+    t.integer "delivery_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +56,6 @@ ActiveRecord::Schema.define(version: 2019_12_12_030825) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
+  add_foreign_key "items", "users"
 end
